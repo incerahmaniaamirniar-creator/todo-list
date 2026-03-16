5e30027f0ca29ffd5673651a8adee10aeb5c729e
incerahmaniaamirniar-creator <incerahmaniaamirniar@gmail.com>
2026-03-16 01:28:16 -0700

    Menambahkan java.script

--- java.js
+++ java.js
@@ -1,35 +1,38 @@
 function addTask(){
 
 let input = document.getElementById("taskInput");
-let taskText = input.value;
+let taskText = input.value.trim();
 
 if(taskText === ""){
-alert("Masukkan tugas dulu!");
+alert("Tugas tidak boleh kosong!");
 return;
 }
 
 let li = document.createElement("li");
 
+let checkbox = document.createElement("input");
+checkbox.type = "checkbox";
+
+checkbox.onclick = function(){
+li.classList.toggle("completed");
+};
+
 let span = document.createElement("span");
 span.textContent = taskText;
 
-span.onclick = function(){
-span.classList.toggle("completed");
-}
+let deleteBtn = document.createElement("button");
+deleteBtn.textContent = "Hapus";
+deleteBtn.className = "delete-btn";
 
-let del = document.createElement("button");
-del.textContent = "🗑";
-del.className="delete";
-
-del.onclick=function(){
+deleteBtn.onclick = function(){
 li.remove();
-}
+};
 
+li.appendChild(checkbox);
 li.appendChild(span);
-li.appendChild(del);
+li.appendChild(deleteBtn);
 
 document.getElementById("taskList").appendChild(li);
 
 input.value="";
-
 }
